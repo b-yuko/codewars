ee94622b3ceacf91c5f8abb2b1e260252ba0873f
b-yuko <yuko.banno.oo@gmail.com>
2025-01-20 17:32:31 +0900

    docs: 問題文追加

--- codewars/src/main/kotlin/codewars8kyu/convertNumberToReversedArrayOfDigits.kt
+++ codewars/src/main/kotlin/codewars8kyu/convertNumberToReversedArrayOfDigits.kt
@@ -1,3 +1,19 @@
+package codewars8kyu
+
+/*
+
+Convert number to reversed array of digits
+数値を桁ごとに分解して逆順の配列に変換する
+
+Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+与えられた非負の数値について、その桁を逆順に並べた配列として返してください。
+
+Example(Input => Output):
+35231 => [1,3,2,5,3]
+0 => [0]
+
+*/
+
 object Kata {
     fun digitize(n:Long):IntArray {
         return n.toString()
